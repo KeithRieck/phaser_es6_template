59932968873ce4ba4a0df974f3570b9ff70226b8
Keith Rieck <KeithRieck@gmail.com>
2019-08-10 17:56:10 -0500

    Specified config type

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { WelcomeScene } from './WelcomeScene'
 import { GameScene } from './GameScene'
 
 const config = {
+    type: Phaser.AUTO,
     width: 640,
     height: 480,
     parent: 'content',
